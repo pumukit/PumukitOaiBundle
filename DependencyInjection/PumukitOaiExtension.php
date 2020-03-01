c7ca2f505ccb53d3b3ee160b727ebf3ba83502c9
Pablo Ogando Ferreira <pabloogando5@gmail.com>
2020-03-01 22:20:27 +0100

    TTK-23485 migrate code to SF4

--- DependencyInjection/PumukitOaiExtension.php
+++ DependencyInjection/PumukitOaiExtension.php
@@ -7,17 +7,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-/**
- * This is the class that loads and manages your bundle configuration.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
 class PumukitOaiExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
@@ -34,7 +26,7 @@ class PumukitOaiExtension extends Extension
         $container->setParameter('pumukitoai.dc_subject_format', $config['dc_subject_format']);
         $container->setParameter('pumukitoai.dc_identifier_url_mapping', $config['dc_identifier_url_mapping']);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('pumukit_oai.yaml');
     }
 }
